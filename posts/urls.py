620dc6efe9e2a09dcc343b4725f4f0764d25d0fc
Artem <artem.boyvan@mail.ru>
2025-11-14 14:30:31 +0300

    Исправлены ошибки

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,5 @@ from .views import group_posts
 
 urlpatterns = [
     path('', views.index),
-    path('group_posts/<slug:slug>/', views.group_posts())
+    path('group_posts/<slug:slug>/', views.group_posts)
 ]
--- posts/views.py
+++ posts/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('Главная страница')
 
-def group_posts(request):
+def group_posts(request,slug):
     return HttpResponse('Умные мысли группа ВК')
 # Create your views here.
